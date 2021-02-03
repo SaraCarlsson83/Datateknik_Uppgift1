daf602b5a5cdc98ef97b9faf4984e6984b986cc5
SaraCarlsson83 <71943956+SaraCarlsson83@users.noreply.github.com>
2021-02-03 11:08:24 +0100

    Add files via upload

---
+++ script0.sql
@@ -0,0 +1,118 @@
+drop database if exists assignment1;
+create database assignment1;
+use assignment1;
+
+create table Community(
+id int not null auto_increment, 
+com_name varchar(50) not null, 
+primary key(id),
+created timestamp default current_timestamp, 
+lastupdate timestamp default current_timestamp on update current_timestamp);
+
+create table Price(
+id int not null auto_increment, 
+value int not null, 
+primary key(id),
+created timestamp default current_timestamp, 
+lastupdate timestamp default current_timestamp on update current_timestamp);
+
+create table Color(
+id int not null auto_increment, 
+col_name varchar(50) not null, 
+primary key(id),
+created timestamp default current_timestamp, 
+lastupdate timestamp default current_timestamp on update current_timestamp);
+
+create table Label(
+id int not null auto_increment, 
+label_name varchar(50) not null, 
+primary key(id),
+created timestamp default current_timestamp, 
+lastupdate timestamp default current_timestamp on update current_timestamp);
+
+create table Rating_Alternatives(
+id int not null auto_increment, 
+options varchar(50) not null, 
+primary key(id),
+created timestamp default current_timestamp, 
+lastupdate timestamp default current_timestamp on update current_timestamp);
+
+create table Category(
+id int not null auto_increment, 
+cat_name varchar(50) not null, 
+primary key(id),
+created timestamp default current_timestamp, 
+lastupdate timestamp default current_timestamp on update current_timestamp);
+
+create table Customer(
+id int not null auto_increment, 
+First_Name varchar(50) not null, 
+Last_Name varchar(50) not null, 
+Adress varchar(50) not null, 
+Community_id int not null,
+primary key(id),
+foreign key (Community_id) references Community(id),
+created timestamp default current_timestamp, 
+lastupdate timestamp default current_timestamp on update current_timestamp);
+
+create table Product(
+id int not null auto_increment, 
+Label_id int not null, 
+Color_id int,  
+Price_id int not null,
+primary key(id),
+foreign key (Label_id) references Label(id),
+foreign key (Color_id) references Color(id),
+foreign key (Price_id) references Price(id),
+created timestamp default current_timestamp, 
+lastupdate timestamp default current_timestamp on update current_timestamp);
+
+create table Shoe(
+id int not null auto_increment, 
+Product_id int not null,
+primary key(id),
+foreign key (Product_id) references Product(id),
+created timestamp default current_timestamp, 
+lastupdate timestamp default current_timestamp on update current_timestamp);
+
+create table Divide_into_Category(
+id int not null auto_increment, 
+Shoe_id int not null,
+Category_id int,
+primary key(id),
+foreign key (Shoe_id) references Shoe(id),
+foreign key (Category_id) references Category(id),
+created timestamp default current_timestamp, 
+lastupdate timestamp default current_timestamp on update current_timestamp);
+
+create table Orders(
+id int not null auto_increment, 
+date Date not null,
+Customer_id int not null,
+primary key(id),
+foreign key (Customer_id) references Customer(id),
+created timestamp default current_timestamp, 
+lastupdate timestamp default current_timestamp on update current_timestamp);
+
+create table Order_includes(
+id int not null auto_increment, 
+Orders_id int not null,
+Product_id int not null,
+primary key(id),
+foreign key (Orders_id) references Orders(id),
+foreign key (Product_id) references Product(id),
+created timestamp default current_timestamp, 
+lastupdate timestamp default current_timestamp on update current_timestamp);
+
+create table Rating(
+id int not null auto_increment, 
+Comment varchar(150),
+RatingAlternatives_id int not null,
+Product_id int not null,
+Customer_id int not null, 
+primary key(id),
+foreign key (RatingAlternatives_id) references Rating_Alternatives(id),
+foreign key (Product_id) references Product(id),
+foreign key (Customer_id) references Customer(id),
+created timestamp default current_timestamp, 
+lastupdate timestamp default current_timestamp on update current_timestamp);
