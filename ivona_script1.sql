ec9a5f3cfe35840512d48e3dd50fc1000a1e8141
Ivona Zoricic <72072576+I-Zor@users.noreply.github.com>
2021-02-03 12:34:01 +0100

    Add files via upload

--- ivona_script1.sql
+++ ivona_script1.sql
@@ -22,9 +22,24 @@ insert into product (Label_id, Color_id, Price_id, Size) values (33,81,124,'44')
 insert into product (Label_id, Color_id, Price_id, Size) values (33,84,120,'46');
 insert into product (Label_id, Color_id, Price_id, Size) values (32,80,124,'41');
 
+alter table shoe AUTO_INCREMENT=50;
+insert into shoe (Product_id) values (40), (41), (42), (43), (44), (45), (46), (47);
+
+alter table divide_into_category AUTO_INCREMENT=60;
+insert into divide_into_category (Shoe_id, Category_id) values (50,72);
+insert into divide_into_category (Shoe_id, Category_id) values (50,75);
+insert into divide_into_category (Shoe_id, Category_id) values (51,70);
+insert into divide_into_category (Shoe_id, Category_id) values (51,74);
+insert into divide_into_category (Shoe_id, Category_id) values (52,70);
+insert into divide_into_category (Shoe_id, Category_id) values (52,75);
+insert into divide_into_category (Shoe_id, Category_id) values (53,74);
+insert into divide_into_category (Shoe_id, Category_id) values (53,75);
+insert into divide_into_category (Shoe_id, Category_id) values (54,72);
+insert into divide_into_category (Shoe_id, Category_id) values (54,74);
+insert into divide_into_category (Shoe_id, Category_id) values (55,73);
+insert into divide_into_category (Shoe_id, Category_id) values (55,72);
+insert into divide_into_category (Shoe_id, Category_id) values (56,73);
+insert into divide_into_category (Shoe_id, Category_id) values (56,70);
+insert into divide_into_category (Shoe_id, Category_id) values (57,71);
 
 
-
-
-select*from price;
-select*from product;
